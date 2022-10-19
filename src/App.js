622889f0f512b9ff65201299da83b8ef76b71dff
Shuhei Yamaguchi <shuhei@SY-MacbookPro-3.local>
2022-10-19 18:26:56 -0400

    Adding Clock Component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //Format 1
+  const name = 'Shuhei';
+  const element = <h1>Hello, {name}</h1>
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  //Format 2
+  const user = {
+    firstName: 'Shuhei',
+    lastName: 'Yamaguchi'
+  };
+  
+  const elem2 = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {elem2}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </header>
     </div>
   );
 }
 
 export default App;
+
+
+// {/* <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+//       {/* <img src={logo} className="App-logo" alt="logo" /> */}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Clock from './Clock';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode><div>
+    {/* <App/> */}
+    <Clock/>
+    </div>
   </React.StrictMode>
 );
 
